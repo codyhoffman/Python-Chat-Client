c491ec4d9a597e99f0d9a997020af80a553d442c
Hoffman <cjhoffman@serv16.divms.uiowa.edu>
2017-03-09 13:29:32 -0600

    updates

--- chat.py
+++ chat.py
@@ -1,6 +1,25 @@
 import sys, os, socket, struct, select, argparse
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("name")
+parser.add_argument("myIPhost")
+parser.add_argument("yourIPhost")
+args = parser.parse_args()
+print('Chat started:', args.name, args.myIPhost, args.yourIPhost)
+
+myName = args.name
+
+myHostPort = args.myIPhost.split(':')
+myHost = myHostPort[0]
+myPort = int(myHostPort[1])
+
+yourHostPort = args.yourIPhost.split(':')
+yourHost = myHostPort[0]
+yourPort = int(myHostPort[1])
+
+# parsing complete time work with socket
+
 def encode_chat_msg(seqnum, UID, DID, msg, version=150):
     header_buf = bytearray(36)
     UID = UID + ' ' * (16 - len(UID))
@@ -21,10 +40,6 @@ def decode_chat_msg(msg_buf):
     return (seqnum, UID, DID, msg)
 
 
-msg = encode_chat_msg(3, 'abc', 'cdef', 'hello')
-msg = decode_chat_msg(msg)[-1]
-print(msg)
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
@@ -42,7 +57,8 @@ try:
 
             print('sending "%s"' % user_input)
             user_input_bytes = user_input.encode('utf-8')
-            sent = sock.sendto(user_input_bytes, server_address)
+            sent = sock.sendto(user_input_bytes, (yourHost, yourPort))
+            print('sent')
 
         if sock in rlist:
             # data is pending on the socket
